cf2c4fd1d49bbb0299e9e79f7757574420db72e3
Nicolás C. Ibarra <ricardoibarra2044@gmail.com>
2022-03-08 09:28:44 -0600

    feat: update rss feeds

--- app/services/RSSFeedService.php
+++ app/services/RSSFeedService.php
@@ -14,18 +14,12 @@ class RSSFeedService {
     $this->fetcher = $fetcher;
   }
 
-  /**
-   * Esta función esta pensada para rellenar el combobox de los RSS Feeds.
-   * Por el momento retorna todas las Feeds con TODAS las noticias, 
-   * propongo dejarlo así con el fin de poner números más interesantes 
-   * en el reporte de VH.
-   */
   function getAll() {
     return $this->rssFeedRepo->findAll();
   }
 
   function getById($rssId) {
-    return $this->rssFeedRepo->findBy(['id' => $rssId]);
+    return $this->rssFeedRepo->findOneBy(['id' => $rssId]);
   }
 
   function addOrGet($url) {
@@ -35,5 +29,16 @@ class RSSFeedService {
     $rssFeed = $this->fetcher->fetchAndParseXmlRssFeed($url);
     $this->rssFeedRepo->save($rssFeed);
     return $this->rssFeedRepo->get($url);
+  }
+
+  function update($selectedRssFeed) {
+    $rssFeeds = $this->getAll();
+    foreach ($rssFeeds as $rssFeed) {
+      $rssFeed->clearNews();
+      $updatedRssFeed = $this->fetcher->fetchAndParseXmlRssFeed($rssFeed->getUrl());
+      $rssFeed->setNews($updatedRssFeed->getNews());
+      $this->rssFeedRepo->save($rssFeed);
+    }
+    return $this->getById($selectedRssFeed);
   }
 }
